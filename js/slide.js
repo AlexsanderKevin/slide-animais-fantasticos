fc847c3b9833179b663e0a0520705a19ca902d41
ChesterKing66 <kevin13348@gmail.com>
2022-07-11 22:15:27 -0300

    aula-1206 Slide Next e Prev

--- js/slide.js
+++ js/slide.js
@@ -47,9 +47,46 @@ export default class Slide {
         this.onFinish = this.onFinish.bind(this);
     }
 
+    slidePosition(slide){
+        const margin  = (this.wrapper.offsetWidth - slide.offsetWidth) / 2
+        console.log(margin);
+
+        return -(slide.offsetLeft - margin);
+    }
+
+    slideConfig(){
+        this.slideArray = [...this.slide.children].map(element => {
+            const position = this.slidePosition(element);
+
+            return {
+                position: position,
+                element: element,
+            }
+        })
+    }
+
+    slideIndexNav(index){
+        const last = this.slideArray.length -1;
+
+        this.index = {
+            prev: index ? index -1 : undefined,
+            active: index,
+            next: index === last ? undefined : index +1,
+        }
+    }
+
+    changeSlide(index){
+        const activeSlide = this.slideArray[index];
+
+        this.moveSlide(activeSlide.position);
+        this.slideIndexNav(index);
+        this.dist.finalPosition = activeSlide.position;
+    }
+
     init(){
         this.bindEvents();
         this.addSlideEvents();
+        this.slideConfig();
         return this;
     }
 }
